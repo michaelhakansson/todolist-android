85dc5c6725509ba1e16cce82ef5b520f76b6acfb
michaelhakansson <michael.hakansson@gmail.com>
2015-03-20 14:30:27 +0100

    Work on receiving list via http request

--- app/src/main/java/se/mhakansson/todolist_android/ListOverview.java
+++ app/src/main/java/se/mhakansson/todolist_android/ListOverview.java
@@ -1,5 +1,6 @@
 package se.mhakansson.todolist_android;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,49 +13,41 @@ import android.widget.TextView;
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class ListOverview extends ActionBarActivity {
 
     private Socket socket;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_overview);
-
-        String[] array = new String[]{"Apple", "Google"};
-        ViewGroup listContainer = (ViewGroup) findViewById(R.id.list_container);
-
-        for (String anArray : array) {
-            TextView text = new TextView(this);
-            final String textToSet = anArray;
-            text.setText(anArray);
-            text.setTextSize(20);
-            text.setClickable(true);
-
-            text.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.d("Click", textToSet);
-                }
-            });
-
-            listContainer.addView(text);
-
-            //CheckBox checkBox = new CheckBox(this);
-            //checkBox.setText(array[i]);
-            //checkboxContainer.addView(checkBox);
-        }
-
-
+    // Establish socket connection to server
+    {
         try {
             socket = IO.socket("http://192.168.0.104:1337");
         } catch (URISyntaxException e) {
             Log.d("error in socket url", "");
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_overview);
+
+        // Download all current lists and add to the view
+        new DownloadLists().execute("http://192.168.0.104:1337/list");
 
         socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 
@@ -62,22 +55,34 @@ public class ListOverview extends ActionBarActivity {
             public void call(Object... args) {
                 Log.d("MainActivity: ", "socket connected");
 
+                // Subscribe to lists by
                 JSONObject obj = new JSONObject();
                 try {
                     obj.put("url", "/lists/subscribe");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
                 socket.emit("get", obj);
+
             }
 
         }).on("listAdded", new Emitter.Listener() {
 
             @Override
             public void call(Object... args) {
+                JSONObject obj = null;
+                try {
+                    obj = new JSONObject(args[0].toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 Log.d("MainActivity: ", "List Added: ");
-                    Log.d("MainActivity: ", args[0].toString());
+                try {
+                    Log.d("List id: ", Integer.toString(obj.getInt("id")));
+                    Log.d("List name: ", obj.getString("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
         }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
@@ -96,7 +101,12 @@ public class ListOverview extends ActionBarActivity {
         socket.connect();
     }
 
-
+    @Override
+    protected void onDestroy() {
+        Log.d("OnDestroy", "Called");
+        super.onDestroy();
+        socket.disconnect();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -119,5 +129,76 @@ public class ListOverview extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    // Downloads all lists asynchronous and loads them into the view
+    class DownloadLists extends AsyncTask<String, String, JSONArray> {
+        @Override
+        protected JSONArray doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            JSONArray responseJson = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            try {
+                responseJson = new JSONArray(responseString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return responseJson;
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray response) {
+            super.onPostExecute(response);
+            JSONObject row = null;
+            ViewGroup listContainer = (ViewGroup) findViewById(R.id.list_container);
+
+            // Handle each list in response
+            for (int i = 0; i < response.length(); ++i) {
+                try {
+                    row = response.getJSONObject(i);
+                    final int id = row.getInt("id");
+                    final String name = row.getString("name");
+
+                    TextView text = new TextView(listContainer.getContext());
+                    text.setId(id);
+                    text.setText(name);
+                    text.setTextSize(20);
+                    text.setClickable(true);
+
+                    text.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Log.d("Click on id ", Integer.toString(id));
+                        }
+                    });
+
+                    listContainer.addView(text);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
 
 }
