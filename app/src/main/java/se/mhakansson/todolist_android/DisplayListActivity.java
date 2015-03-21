1a8cf0cddc9c718353c35d842b121ee40f090044
michaelhakansson <michael.hakansson@gmail.com>
2015-03-21 17:41:11 +0100

    Initial work with RecyclerView in the display list view

--- app/src/main/java/se/mhakansson/todolist_android/DisplayListActivity.java
+++ app/src/main/java/se/mhakansson/todolist_android/DisplayListActivity.java
@@ -4,11 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 
 public class DisplayListActivity extends ActionBarActivity {
+
+    private RecyclerView mRecyclerView;
+    private CustomRecyclerAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private EditText mText;
+    private ArrayList<ListItem> mData = new ArrayList<>();
 
 
     @Override
@@ -16,12 +27,52 @@ public class DisplayListActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_list);
 
+        // Initializing views.
+        mText = (EditText) findViewById(R.id.textEt);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
 
-        Intent intent = getIntent();
-        int listId = intent.getIntExtra(ListOverview.LIST_ID, -1);
-        TextView textView = new TextView(this);
-        textView.setText(Integer.toString(listId));
-        setContentView(textView);
+        // Setting the LayoutManager.
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Setting the adapter.
+        mAdapter = new CustomRecyclerAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Add data locally to the list.
+        ListItem listToAdd = new ListItem(1, "Texten", false);
+        ListItem listToAdd2 = new ListItem(1, "Texten", false);
+        ListItem listToAdd3 = new ListItem(1, "Texten", false);
+        ListItem listToAdd4 = new ListItem(1, "Texten", false);
+        ListItem listToAdd5 = new ListItem(1, "Texten", false);
+        ListItem listToAdd6 = new ListItem(1, "Texten", false);
+
+        ArrayList<ListItem> lst = new ArrayList<ListItem>();
+        lst.add(listToAdd);
+        lst.add(listToAdd2);
+        lst.add(listToAdd3);
+        lst.add(listToAdd4);
+        lst.add(listToAdd5);
+        lst.add(listToAdd6);
+
+        mData.add(listToAdd);
+        mData.add(listToAdd2);
+        mData.add(listToAdd3);
+        mData.add(listToAdd4);
+        mData.add(listToAdd5);
+        mData.add(listToAdd6);
+
+
+        // Update adapter
+//        mAdapter.addItem(mData.size()-1, listToAdd);
+        mAdapter.updateList(lst);
+
+
+//        Intent intent = getIntent();
+//        int listId = intent.getIntExtra(ListOverviewActivity.LIST_ID, -1);
+//        TextView textView = new TextView(this);
+//        textView.setText(Integer.toString(listId));
+//        setContentView(textView);
     }
 
 
@@ -39,7 +90,6 @@ public class DisplayListActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 
 }
 
